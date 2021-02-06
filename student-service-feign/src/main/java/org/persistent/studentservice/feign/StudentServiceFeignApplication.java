cc5e2e46b47dbdbe909ce15695b78d97d5066810
Koitha Perera <kolitha_perera@persistent.com>
2021-02-06 19:56:10 +0530

    Added the project for feign client.

---
+++ student-service-feign/src/main/java/org/persistent/studentservice/feign/StudentServiceFeignApplication.java
@@ -0,0 +1,13 @@
+package org.persistent.studentservice.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentServiceFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentServiceFeignApplication.class, args);
+	}
+
+}
---
+++ student-service-feign/src/test/java/org/persistent/studentservice/feign/StudentServiceFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package org.persistent.studentservice.feign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServiceFeignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
