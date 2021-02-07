e4c65a8141e4f9161c708487c24f705a57db4a18
Koitha Perera <kolitha_perera@persistent.com>
2021-02-07 13:06:18 +0530

    Added the API gateway.

---
+++ student-service-api-gateway/src/main/java/org/persistent/studentservice/studentserviceapigateway/StudentServiceApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.persistent.studentservice.studentserviceapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentServiceApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentServiceApiGatewayApplication.class, args);
+	}
+
+}
---
+++ student-service-api-gateway/src/test/java/org/persistent/studentservice/studentserviceapigateway/StudentServiceApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.persistent.studentservice.studentserviceapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServiceApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
