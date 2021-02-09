98d2fc9cb5313c25ef7044a4f5c9941f6c2dd118
Koitha Perera <kolitha_perera@persistent.com>
2021-02-09 17:50:17 +0530

    Added the configuration server.

---
+++ student-service-config/src/main/java/org/persistent/studentservice/config/StudentServiceConfigApplication.java
@@ -0,0 +1,13 @@
+package org.persistent.studentservice.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentServiceConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentServiceConfigApplication.class, args);
+	}
+
+}
---
+++ student-service-config/src/test/java/org/persistent/studentservice/config/StudentServiceConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package org.persistent.studentservice.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServiceConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
