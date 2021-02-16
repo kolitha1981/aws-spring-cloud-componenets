16cca29010a8f76636e28d0c396f8604d6746aae
Koitha Perera <kolitha_perera@persistent.com>
2021-02-16 07:48:03 +0530

    Fixed runtime errors.

--- student-service-kafka/src/main/java/org/persistent/studentservice/kafka/config/KafkaConfig.java
+++ student-service-kafka/src/main/java/org/persistent/studentservice/kafka/config/KafkaConfig.java
@@ -3,14 +3,19 @@ package org.persistent.studentservice.kafka.config;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+
+import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
 
 @Configuration
 public class KafkaConfig {
@@ -25,6 +30,16 @@ public class KafkaConfig {
 		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		return new DefaultKafkaProducerFactory<>(configProps);
+	}
+
+	@Bean
+	public ConsumerFactory<String, String> consumerFactory() {
+		Map<String, Object> configProps = new HashMap<>();
+		configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+		configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
+		configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		return new DefaultKafkaConsumerFactory<>(configProps);
 	}
 
 	@Bean
--- student-service-kafka/src/main/java/org/persistent/studentservice/kafka/controller/KakaPublishController.java
+++ student-service-kafka/src/main/java/org/persistent/studentservice/kafka/controller/KakaPublishController.java
@@ -3,19 +3,25 @@ package org.persistent.studentservice.kafka.controller;
 import org.persistent.studentservice.common.Student;
 import org.persistent.studentservice.kafka.domain.PublishingStatus;
 import org.persistent.studentservice.kafka.producer.KafkaPublishingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 
 @RestController
 public class KakaPublishController {
 
+	@Autowired
 	private KafkaPublishingService kafkaPublishingService;
 
-	@PostMapping(path = "/publish")
+	@PostMapping(path = "/publish", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
 	public ResponseEntity<PublishingStatus> publish(@RequestBody Student student) {
 		final PublishingStatus publishingStatus = this.kafkaPublishingService.publish(student);
 		if (publishingStatus == PublishingStatus.STATUS_FAILED) {
