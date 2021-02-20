19adc22f0ed158ab0d913ccef890131e5eb2bfce
Koitha Perera <kolitha_perera@persistent.com>
2021-02-20 11:58:36 +0530

    Added the spring cloud implementation for kafka.

---
+++ student-service-cloud-kafka/src/main/java/org/persistent/studentservice/kafka/StudentServiceCloudKafkaApplication.java
@@ -0,0 +1,13 @@
+package org.persistent.studentservice.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentServiceCloudKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentServiceCloudKafkaApplication.class, args);
+	}
+
+}
---
+++ student-service-cloud-kafka/src/test/java/org/persistent/studentservice/kafka/StudentServiceCloudKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.persistent.studentservice.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServiceCloudKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
